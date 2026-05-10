68f62d27d979adc53ec4da91e80c8b6a5e34995f
Sanjana Bodapati <sanjanaradhya7383@gmail.com>
2026-05-10 21:25:38 +0530

    render deployment

--- server/server.js
+++ server/server.js
@@ -29,11 +29,12 @@ mongoose
   .then(() => {
     console.log("MongoDB Connected");
 
-   const PORT = process.env.PORT || 8080;
+   const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});  })
+});
+  })
   .catch((err) => {
     console.log(err);
   });
