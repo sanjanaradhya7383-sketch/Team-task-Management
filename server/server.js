33b9a4228645b25e8bd1d2841a390c4b9af13eea
Sanjana Bodapati <sanjanaradhya7383@gmail.com>
2026-05-10 21:10:45 +0530

    fix railway networking

--- server/server.js
+++ server/server.js
@@ -29,12 +29,11 @@ mongoose
   .then(() => {
     console.log("MongoDB Connected");
 
-   const PORT = process.env.PORT || 5000;
+   const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
-});
-  })
+});  })
   .catch((err) => {
     console.log(err);
   });
